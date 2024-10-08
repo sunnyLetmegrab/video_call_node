5bab8c8cc987a9ff0e4af1a13a530cf6339e1205
sunny <sunny@letmegrab.com>
2024-10-08 18:37:38 +0530

    home page setup.

--- src/app.mjs
+++ src/app.mjs
@@ -2,12 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import adminRoute from './routes/admin_routes.mjs';
 import userRoute from './routes/user_routes.mjs';
-
+import path from 'path'
+import { fileURLToPath } from 'url'
 var app = express()
 
 
 app.use(bodyParser.json())
-
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to 
+const __dirname = path.dirname(__filename);
+app.use("/public", express.static(path.join(__dirname, 'public/')));
 
 
 app.use('/app/admin', adminRoute)
